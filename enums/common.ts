de10cd6f9787af2f22b11c0ec9f7aa781a77c9e1
Joshtri <stuffofyos1516@gmail.com>
2025-10-31 09:16:41 +0700

    Commit: common.ts

---
+++ enums/common.ts
@@ -0,0 +1,13 @@
+export enum ReactionType {
+    LIKE = "LIKE",
+    LOVE = "LOVE",
+    LAUGH = "LAUGH",
+    WOW = "WOW",
+    SAD = "SAD",
+    ANGRY = "ANGRY"
+}
+
+export enum UserRole {
+    ADMIN = "ADMIN",
+    VISITOR = "VISITOR"
+}
