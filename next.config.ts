1e29d6e03e8aacb9b780946a5bb6698117b36c14
Joshtri <stuffofyos1516@gmail.com>
2025-10-31 09:16:42 +0700

    Commit: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+ 
+  images: {
+    dangerouslyAllowLocalIP: true,
+    disableStaticImages: true,
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ]
+  }
 };
 
 export default nextConfig;
