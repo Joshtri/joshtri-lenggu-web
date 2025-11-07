8f41df351a1579b03bf73a697065139c66adf495
Joshtri <stuffofyos1516@gmail.com>
2025-11-07 18:53:38 +0700

    Commit: posts.schema.ts

--- db/schema/posts.schema.ts
+++ db/schema/posts.schema.ts
@@ -1,16 +1,20 @@
-import { integer, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { sql } from 'drizzle-orm';
+import { pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
 import { timestamps } from "./columns.helpers";
 import { labels } from "./labels.schema";
+import { types } from "./type.schema";
 import { users } from "./users.schema";
 
+
 export const posts = pgTable('posts', {
-    id: serial('id').primaryKey().notNull(),
+    id: uuid().default(sql`gen_random_uuid()`).primaryKey(),
     slug: varchar('slug', { length: 255 }).notNull().unique(),
     title: varchar('title', { length: 255 }).notNull(),
     coverImage: varchar('cover_image', { length: 500 }).notNull(),
     content: text('content').notNull(),
     excerpt: text('excerpt').notNull(),
-    authorId: integer('author_id').references(() => users.id),
-    labelId: integer('label_id').references(() => labels.id),
+    authorId: uuid('author_id').references(() => users.id).notNull(),
+    labelId: uuid('label_id').references(() => labels.id).notNull(),
+    typeId: uuid('type_id').references(() => types.id).notNull(),
     ...timestamps
 });
