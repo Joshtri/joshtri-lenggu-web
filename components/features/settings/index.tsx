59d6d36aee704c4851baa01e662ab8dd312eaf99
Joshtri <stuffofyos1516@gmail.com>
2025-11-07 18:53:36 +0700

    Commit: index.tsx

---
+++ components/features/settings/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function SettingsGrid() {
+  return (
+    //all the logic condition render will be happend on here. 
+    <div>SettingsGrid</div>
+  )
+}
