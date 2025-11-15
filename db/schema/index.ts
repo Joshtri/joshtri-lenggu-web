36a1183333e617fa5a86146579619e4dff29646a
Joshtri <stuffofyos1516@gmail.com>
2025-11-16 01:43:39 +0700

    Commit: index.ts

--- db/schema/index.ts
+++ db/schema/index.ts
@@ -2,6 +2,7 @@ export * from "./posts.schema";
 export * from "./labels.schema";
 export * from "./users.schema";
 export * from "./comments.schema";
+export * from "./post_views.schema";
 export * from "./relations";
 export * from "./columns.helpers";
 export * from "./type.schema";
